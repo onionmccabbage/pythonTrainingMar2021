5ead55be6eecef3b222240a08683dcdbc710fc4f
onionmccabbage <onionmccabbage@live.com>
2021-03-09 08:56:34 +0000

    Add files via upload

--- by_ref_by_val.py
+++ by_ref_by_val.py
@@ -12,7 +12,7 @@ n = m.copy() # no longer by reference
 p = m # by REFERENCE
 m[1] = 77
 
-print(m,n)
+print(m,n,p) # m and p point to the same structure in memory
 
 # calling __main__
 if __name__ == "__main__": # several things in Pyton use double underscores like this
